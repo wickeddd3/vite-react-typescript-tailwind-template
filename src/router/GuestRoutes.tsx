ab3b86b4a36a9cf470a69b93fe579e5fb2db2069
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-03 19:54:07 +0800

    feat: add GuestRoutes

---
+++ src/router/GuestRoutes.tsx
@@ -0,0 +1,27 @@
+import { lazy } from "react";
+import { createBrowserRouter } from "react-router-dom";
+import { MainLayout } from "@/components/layouts/main-layout";
+import { PageLoader } from "@/components/PageLoader";
+
+// Delay function
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// Create a lazy-loaded Login component with a delay
+const Login = lazy(() => delay(1000).then(() => import("@/pages/auth/Login")));
+
+export const GuestRoutes = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "",
+        element: (
+          <PageLoader>
+            <Login />
+          </PageLoader>
+        ),
+      },
+    ],
+  },
+]);
