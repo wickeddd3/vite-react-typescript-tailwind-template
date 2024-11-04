07b28bc93c156d318c5d0c07a1ceefa72e3145dc
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-05 00:04:26 +0800

    feat: add DataTable page

---
+++ src/pages/tables/DataTable.tsx
@@ -0,0 +1,13 @@
+import { Page } from "@/components/layouts/Page";
+
+const DataTable = () => {
+  return (
+    <Page>
+      <Page.Navbar title="DataTable" />
+      <Page.Breadcrumb className="px-4" />
+      <Page.Content>DataTable</Page.Content>
+    </Page>
+  );
+};
+
+export default DataTable;
