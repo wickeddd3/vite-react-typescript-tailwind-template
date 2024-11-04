0dedd50bef3b50db740d8c8e23ced342610b65b4
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-04 23:59:25 +0800

    feat: add Contact page

---
+++ src/pages/forms/Contact.tsx
@@ -0,0 +1,13 @@
+import { Page } from "@/components/layouts/Page";
+
+const Contact = () => {
+  return (
+    <Page>
+      <Page.Navbar title="Contact" />
+      <Page.Breadcrumb className="px-4" />
+      <Page.Content>Contact</Page.Content>
+    </Page>
+  );
+};
+
+export default Contact;
