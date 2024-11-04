e7dcf824a17d6fa61bf7c94dd9a11c71c829728a
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-04 21:25:16 +0800

    feat: use Page component in Dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,13 @@
 import { Page } from "@/components/layouts/Page";
 
 const Dashboard = () => {
-  return <Page>dashboard</Page>;
+  return (
+    <Page>
+      <Page.Navbar title="Dashboard" />
+      <Page.Breadcrumb className="px-4" />
+      <Page.Content>Dashboard</Page.Content>
+    </Page>
+  );
 };
 
 export default Dashboard;
