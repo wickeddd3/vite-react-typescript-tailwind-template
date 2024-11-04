20e86441e4ff9abc448ea456faeafb0b766ef2fa
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-04 21:25:26 +0800

    feat: use Page component in Analytics page

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -1,7 +1,13 @@
 import { Page } from "@/components/layouts/Page";
 
 const Analytics = () => {
-  return <Page>analytics</Page>;
+  return (
+    <Page>
+      <Page.Navbar title="Dashboard" />
+      <Page.Breadcrumb className="px-4" />
+      <Page.Content>Analytics</Page.Content>
+    </Page>
+  );
 };
 
 export default Analytics;
