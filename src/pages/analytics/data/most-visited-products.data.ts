beef8b748e51ee004811b73a5f7d9ced7e930571
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-06 03:12:29 +0800

    feat: add most-visited-products.data.ts

---
+++ src/pages/analytics/data/most-visited-products.data.ts
@@ -0,0 +1,24 @@
+import { ChartConfig } from "@/components/ui/chart";
+
+export const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+];
+
+export const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "hsl(var(--chart-2))",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "hsl(var(--chart-2))",
+  },
+  label: {
+    color: "hsl(var(--background))",
+  },
+} satisfies ChartConfig;
