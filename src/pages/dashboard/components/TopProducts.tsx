032d721e9ac7c2a21089e7c80d364602904b82cb
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-05 23:02:55 +0800

    feat: add TopProducts

---
+++ src/pages/dashboard/components/TopProducts.tsx
@@ -0,0 +1,89 @@
+import { useMemo } from "react";
+import { TrendingUp } from "lucide-react";
+import { Label, Pie, PieChart, ResponsiveContainer } from "recharts";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { chartConfig, chartData } from "../data/top-products.data";
+
+export const TopProducts = () => {
+  const totalSales = useMemo(() => {
+    return chartData.reduce((acc, curr) => acc + curr.sales, 0);
+  }, []);
+
+  return (
+    <Card>
+      <CardHeader className="items-center pb-0">
+        <CardTitle>Top Products</CardTitle>
+        <CardDescription>January - December 2023</CardDescription>
+      </CardHeader>
+      <CardContent className="flex-1 pb-0">
+        <ResponsiveContainer width="100%" height="240px">
+          <ChartContainer config={chartConfig}>
+            <PieChart>
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel />}
+              />
+              <Pie
+                data={chartData}
+                dataKey="sales"
+                nameKey="phone"
+                innerRadius={60}
+                strokeWidth={5}
+              >
+                <Label
+                  content={({ viewBox }) => {
+                    if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                      return (
+                        <text
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          textAnchor="middle"
+                          dominantBaseline="middle"
+                        >
+                          <tspan
+                            x={viewBox.cx}
+                            y={viewBox.cy}
+                            className="fill-foreground text-3xl font-bold"
+                          >
+                            {totalSales.toLocaleString()}
+                          </tspan>
+                          <tspan
+                            x={viewBox.cx}
+                            y={(viewBox.cy || 0) + 24}
+                            className="fill-muted-foreground"
+                          >
+                            Sales
+                          </tspan>
+                        </text>
+                      );
+                    }
+                  }}
+                />
+              </Pie>
+            </PieChart>
+          </ChartContainer>
+        </ResponsiveContainer>
+      </CardContent>
+      <CardFooter className="flex-col gap-2 text-sm">
+        <div className="flex items-center gap-2 font-medium leading-none">
+          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="leading-none text-muted-foreground">
+          Showing total sales of top 5 products
+        </div>
+      </CardFooter>
+    </Card>
+  );
+};
