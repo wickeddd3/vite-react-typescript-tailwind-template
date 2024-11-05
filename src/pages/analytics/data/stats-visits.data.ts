97ba46a5b8e29a15b134836898a68e49b772302d
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-06 03:09:36 +0800

    feat: add stats-visits.data

---
+++ src/pages/analytics/data/stats-visits.data.ts
@@ -0,0 +1,15 @@
+import { ChartConfig } from "@/components/ui/chart";
+
+export const chartData = [
+  { browser: "safari", visitors: 1260, fill: "var(--color-safari)" },
+];
+
+export const chartConfig = {
+  visitors: {
+    label: "Visitors",
+  },
+  safari: {
+    label: "Safari",
+    color: "hsl(var(--chart-2))",
+  },
+} satisfies ChartConfig;
