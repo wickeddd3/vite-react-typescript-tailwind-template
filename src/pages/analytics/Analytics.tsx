c80c1ba408136462829d58f5d89561a2f9f21b3d
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-06 03:13:03 +0800

    feat: add AudiencesMetrics and MostVisitedProducts in Analytics page

--- src/pages/analytics/Analytics.tsx
+++ src/pages/analytics/Analytics.tsx
@@ -1,5 +1,7 @@
 import { Page } from "@/components/layouts/Page";
 import { StatsCards } from "@/pages/analytics/components/stats-cards/StatsCards";
+import { AudiencesMetrics } from "@/pages/analytics/components/AudiencesMetrics";
+import { MostVisitedProducts } from "@/pages/analytics/components/MostVisitedProducts";
 
 const Analytics = () => {
   return (
@@ -7,6 +9,14 @@ const Analytics = () => {
       <Page.Navbar title="Analytics" />
       <Page.Breadcrumb className="px-4" />
         <StatsCards />
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+          <div className="col-span-4 md:col-span-2 lg:col-span-2">
+            <AudiencesMetrics />
+          </div>
+          <div className="col-span-4 md:col-span-2 lg:col-span-2">
+            <MostVisitedProducts />
+          </div>
+        </div>
     </Page>
   );
 };
