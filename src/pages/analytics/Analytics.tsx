7f9e8ab19b57e8bd953b7843b080f4da410483f8
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-05 23:06:19 +0800

    add: move Analytics page to its dedicated folder

--- src/pages/analytics/Analytics.tsx
+++ src/pages/analytics/Analytics.tsx
@@ -3,7 +3,7 @@ import { Page } from "@/components/layouts/Page";
 const Analytics = () => {
   return (
     <Page>
-      <Page.Navbar title="Dashboard" />
+      <Page.Navbar title="Analytics" />
       <Page.Breadcrumb className="px-4" />
       <Page.Content>Analytics</Page.Content>
     </Page>
