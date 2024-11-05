db349d684f9a1b1354daf1b724660824c9ac351c
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-06 03:13:41 +0800

    feat: add DeviceMetrics in Analytics page

--- src/pages/analytics/Analytics.tsx
+++ src/pages/analytics/Analytics.tsx
@@ -2,12 +2,14 @@ import { Page } from "@/components/layouts/Page";
 import { StatsCards } from "@/pages/analytics/components/stats-cards/StatsCards";
 import { AudiencesMetrics } from "@/pages/analytics/components/AudiencesMetrics";
 import { MostVisitedProducts } from "@/pages/analytics/components/MostVisitedProducts";
+import { DeviceMetrics } from "@/pages/analytics/components/DeviceMetrics";
 
 const Analytics = () => {
   return (
     <Page>
       <Page.Navbar title="Analytics" />
       <Page.Breadcrumb className="px-4" />
+      <Page.Content>
         <StatsCards />
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
           <div className="col-span-4 md:col-span-2 lg:col-span-2">
@@ -17,6 +19,8 @@ const Analytics = () => {
             <MostVisitedProducts />
           </div>
         </div>
+        <DeviceMetrics />
+      </Page.Content>
     </Page>
   );
 };
