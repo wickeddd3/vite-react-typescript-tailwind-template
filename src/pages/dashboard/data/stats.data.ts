af7be2f056c320f54a462d3c84ab96c4fdf2b619
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-05 23:02:08 +0800

    feat: add stats.data

---
+++ src/pages/dashboard/data/stats.data.ts
@@ -0,0 +1,28 @@
+import { Activity, CreditCard, DollarSign, Users } from "lucide-react";
+
+export const stats = [
+  {
+    title: "Total Revenue",
+    subTitle: "+20.1% from last month",
+    total: "$45,231.89",
+    icon: DollarSign,
+  },
+  {
+    title: "Subscriptions",
+    subTitle: "+180.1% from last month",
+    total: "+2350",
+    icon: Users,
+  },
+  {
+    title: "Sales",
+    subTitle: "+19% from last month",
+    total: "+12,234",
+    icon: CreditCard,
+  },
+  {
+    title: "Active Now",
+    subTitle: "+201 since last hour",
+    total: "+573",
+    icon: Activity,
+  },
+];
