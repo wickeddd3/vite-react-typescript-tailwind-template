cf67a55b0d0158fc26bf1d2dd961caa0fbafdfe7
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-24 00:47:38 +0800

    feat: add CategoryList and wrap Page.Breadcrumb in Page.Navbar

--- src/pages/ecommerce/categories/Categories.tsx
+++ src/pages/ecommerce/categories/Categories.tsx
@@ -1,8 +1,11 @@
-import { Page } from "@/components/layouts/Page";
-import { listCategoriesThunk } from "@/store/slices/categories";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/store";
-import { useEffect } from "react";
+import { listCategoriesThunk } from "@/store/slices/categories";
+import { Page } from "@/components/layouts/Page";
+import { CategorySearch } from "@/pages/ecommerce/categories/components/CategorySearch";
+import { Button } from "@/components/ui/button";
+import { CategoryList } from "@/pages/ecommerce/categories/components/CategoryList";
 
 const Categories = () => {
   const dispatch: AppDispatch = useDispatch();
@@ -17,13 +20,18 @@ const Categories = () => {
 
   return (
     <Page>
-      <Page.Navbar title="Categories" />
-      <Page.Breadcrumb className="px-4" />
+      <Page.Navbar>
+        <Page.Breadcrumb />
+      </Page.Navbar>
       <Page.Content>
-        Categories
-        {categories.map(({ id, name }) => (
-          <p key={id}>{name}</p>
-        ))}
+        <div className="flex items-center justify-between py-2">
+          <span className="text-xl font-bold text-gray-800">Categories</span>
+          <div className="flex items-center gap-2">
+            <CategorySearch />
+            <Button>Add Category</Button>
+          </div>
+        </div>
+        <CategoryList categories={categories} />
       </Page.Content>
     </Page>
   );
