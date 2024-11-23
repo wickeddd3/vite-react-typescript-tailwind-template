4ef0faf8a5fac8632bb3a8e88f61cce4ee29280e
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-23 18:22:56 +0800

    feat: dispatch listCategoriesThunk

--- src/pages/ecommerce/categories/Categories.tsx
+++ src/pages/ecommerce/categories/Categories.tsx
@@ -1,6 +1,13 @@
 import { Page } from "@/components/layouts/Page";
+import { listCategoriesThunk } from "@/store/slices/categories";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "@/store";
 
 const Categories = () => {
+  const dispatch: AppDispatch = useDispatch();
+
+  dispatch(listCategoriesThunk());
+
   return (
     <Page>
       <Page.Navbar title="Categories" />
