5b15231c7ddd82b12045534b355611349d2ec2eb
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-23 18:34:23 +0800

    feat: fetch and list categories

--- src/pages/ecommerce/categories/Categories.tsx
+++ src/pages/ecommerce/categories/Categories.tsx
@@ -1,18 +1,30 @@
 import { Page } from "@/components/layouts/Page";
 import { listCategoriesThunk } from "@/store/slices/categories";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "@/store";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/store";
+import { useEffect } from "react";
 
 const Categories = () => {
   const dispatch: AppDispatch = useDispatch();
 
-  dispatch(listCategoriesThunk());
+  const categories = useSelector(
+    (state: RootState) => state.categoriesSlice.categories.data
+  );
+
+  useEffect(() => {
+    dispatch(listCategoriesThunk());
+  }, [dispatch]);
 
   return (
     <Page>
       <Page.Navbar title="Categories" />
       <Page.Breadcrumb className="px-4" />
-      <Page.Content>Categories</Page.Content>
+      <Page.Content>
+        Categories
+        {categories.map(({ id, name }) => (
+          <p key={id}>{name}</p>
+        ))}
+      </Page.Content>
     </Page>
   );
 };
