ecb6b412688c84934499c5604ffd77945d7e6d80
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-30 22:32:27 +0800

    feat: pass token from state management to Router and set auth token to state management from local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import { Router } from "@/router/Router";
 import { Toaster } from "@/components/ui/toaster";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getLocalStorageState } from "@/utils/local-storage";
+import { tokenName } from "@/config/app.config";
+import { AppDispatch, RootState } from "@/store";
+import { setAuthToken } from "@/store/slices/auth";
 
 const App = () => {
+  const dispatch: AppDispatch = useDispatch();
+
+  const token = useSelector((state: RootState) => state.authSlice.auth?.token);
+
+  useEffect(() => {
+    dispatch(setAuthToken(getLocalStorageState(tokenName)));
+  }, [dispatch]);
+
   return (
     <>
-      <Router token="" />
+      <Router token={token} />
       <Toaster />
     </>
   );
