4c5253a96b1a705a63f6a193c2b78684d8fe598a
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-30 22:27:23 +0800

    feat: add setAuthTokenReducer

--- src/store/slices/auth/index.ts
+++ src/store/slices/auth/index.ts
@@ -2,15 +2,20 @@ import { createSlice } from "@reduxjs/toolkit";
 import { initialState } from "@/store/slices/auth/state";
 import { registerThunk, loginThunk } from "@/store/slices/auth/thunks/auth";
 import { authReducers } from "@/store/slices/auth/extra-reducers/auth";
+import { setAuthTokenReducer } from "@/store/slices/auth/reducers";
 
 export const authSlice = createSlice({
   name: "authSlice",
   initialState,
-  reducers: {},
+  reducers: {
+    setAuthToken: setAuthTokenReducer,
+  },
   extraReducers(builder) {
     authReducers(builder);
   },
 });
+
+export const { setAuthToken } = authSlice.actions;
 
 export { registerThunk, loginThunk };
 
