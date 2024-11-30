afac1698e286f101081534ea98704b7a9af3fe22
wickeddd3 <pablophilipandrew@gmail.com>
2024-12-01 02:19:53 +0800

    feat: wrap MainLayout with AuthGuard

--- src/components/layouts/main-layout/MainLayout.tsx
+++ src/components/layouts/main-layout/MainLayout.tsx
@@ -1,14 +1,17 @@
 import { MainSidebar } from "@/components/layouts/main-layout/MainSidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { Outlet } from "react-router-dom";
+import { AuthGuard } from "@/components/layouts/AuthGuard";
 
 export const MainLayout = () => {
   return (
-    <SidebarProvider>
-      <MainSidebar />
-      <SidebarInset>
-        <Outlet />
-      </SidebarInset>
-    </SidebarProvider>
+    <AuthGuard>
+      <SidebarProvider>
+        <MainSidebar />
+        <SidebarInset>
+          <Outlet />
+        </SidebarInset>
+      </SidebarProvider>
+    </AuthGuard>
   );
 };
