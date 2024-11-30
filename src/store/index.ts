809edf6da16f9359a6479c3dec7bc334117daa87
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-30 18:53:55 +0800

    feat: add auth slice to main store and add AppThunk type

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "@/store/slices/auth";
 import categoriesSlice from "@/store/slices/categories";
+import { ThunkAction, Action } from "@reduxjs/toolkit";
 
 export const store = configureStore({
   reducer: {
+    authSlice: authSlice,
     categoriesSlice: categoriesSlice,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
 
 export default store;
