64b18b28944476c45a2abd3d57660389c520b79b
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-30 15:12:36 +0800

    feat: add register route

--- src/router/GuestRoutes.tsx
+++ src/router/GuestRoutes.tsx
@@ -1,24 +1,37 @@
 import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
-import { MainLayout } from "@/components/layouts/main-layout/MainLayout";
+// import { MainLayout } from "@/components/layouts/main-layout/MainLayout";
 import { PageLoader } from "@/components/PageLoader";
 
 // Delay function
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 // Create a lazy-loaded Login component with a delay
-const Login = lazy(() => delay(1000).then(() => import("@/pages/auth/Login")));
+const Login = lazy(() =>
+  delay(1000).then(() => import("@/pages/auth/login/Login"))
+);
+const Register = lazy(() =>
+  delay(1000).then(() => import("@/pages/auth/register/Register"))
+);
 
 export const GuestRoutes = createBrowserRouter([
   {
     path: "/",
-    element: <MainLayout />,
+    // element: <MainLayout />,
     children: [
       {
         path: "",
         element: (
           <PageLoader>
             <Login />
+          </PageLoader>
+        ),
+      },
+      {
+        path: "/register",
+        element: (
+          <PageLoader>
+            <Register />
           </PageLoader>
         ),
       },
