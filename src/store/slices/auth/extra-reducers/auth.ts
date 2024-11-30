5441bc29666ee46e862af4b0f95d6d7f36eb334c
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-30 20:22:30 +0800

    feat: add loginThunk extra-reducer

--- src/store/slices/auth/extra-reducers/auth.ts
+++ src/store/slices/auth/extra-reducers/auth.ts
@@ -1,10 +1,16 @@
 import { ActionReducerMapBuilder } from "@reduxjs/toolkit";
 import { AuthState } from "@/store/slices/auth/types";
-import { registerThunk } from "@/store/slices/auth/thunks/auth";
+import { loginThunk, registerThunk } from "@/store/slices/auth/thunks/auth";
 
 export const authReducers = (builder: ActionReducerMapBuilder<AuthState>) => {
   // register user
   builder.addCase(registerThunk.fulfilled, (state, action) => {
+    const auth = action.payload || null;
+    state.auth = auth;
+  });
+
+  // login user
+  builder.addCase(loginThunk.fulfilled, (state, action) => {
     const auth = action.payload || null;
     state.auth = auth;
   });
