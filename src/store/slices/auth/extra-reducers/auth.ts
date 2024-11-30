bfe13e7c6269d7fac9bbeba78273b70fcd25647b
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-30 22:28:48 +0800

    feat: store auth token in localstorage after register and login

--- src/store/slices/auth/extra-reducers/auth.ts
+++ src/store/slices/auth/extra-reducers/auth.ts
@@ -1,17 +1,25 @@
 import { ActionReducerMapBuilder } from "@reduxjs/toolkit";
 import { AuthState } from "@/store/slices/auth/types";
 import { loginThunk, registerThunk } from "@/store/slices/auth/thunks/auth";
+import { tokenName } from "@/config/app.config";
+import { setLocalStorageState } from "@/utils/local-storage";
 
 export const authReducers = (builder: ActionReducerMapBuilder<AuthState>) => {
   // register user
   builder.addCase(registerThunk.fulfilled, (state, action) => {
     const auth = action.payload || null;
     state.auth = auth;
+    if (auth?.token) {
+      setLocalStorageState(tokenName, auth?.token);
+    }
   });
 
   // login user
   builder.addCase(loginThunk.fulfilled, (state, action) => {
     const auth = action.payload || null;
     state.auth = auth;
+    if (auth?.token) {
+      setLocalStorageState(tokenName, auth?.token);
+    }
   });
 };
