f1e2c1ba97fe4fe7103f07956e5e40a0ceba927d
wickeddd3 <pablophilipandrew@gmail.com>
2024-11-30 20:22:09 +0800

    feat: add login service

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,9 +1,13 @@
 import { apiRequest } from "@/lib/axios";
-import { RegisterSchemaType } from "@/schema/auth";
+import { LoginSchemaType, RegisterSchemaType } from "@/schema/auth";
 
 const baseUrl = "/api/auth";
 const authResource = apiRequest({ url: "/api/auth" });
 
 export const register = async (data: RegisterSchemaType) => {
   return await authResource.post(data, { url: `${baseUrl}/register` });
+};
+
+export const login = async (data: LoginSchemaType) => {
+  return await authResource.post(data, { url: `${baseUrl}/login` });
 };
