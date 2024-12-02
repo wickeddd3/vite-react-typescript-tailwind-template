f1ab142695b55b085dcb6c7e91f7ce87b5f33471
wickeddd3 <pablophilipandrew@gmail.com>
2024-12-02 23:49:25 +0800

    feat: add delay util

---
+++ src/utils/delay.ts
@@ -0,0 +1,16 @@
+export const debounce = <T extends (...args: unknown[]) => void>(
+  func: T,
+  delay: number
+): ((...args: Parameters<T>) => void) => {
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+
+  return (...args: Parameters<T>) => {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+};
