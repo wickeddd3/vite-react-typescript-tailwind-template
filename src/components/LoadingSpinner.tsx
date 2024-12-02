ecd0a1757ee9e8b59311f9ecfd3f95f485830379
wickeddd3 <pablophilipandrew@gmail.com>
2024-12-02 23:47:24 +0800

    feat: decrease the size and increase the border

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ export const LoadingSpinner = () => {
   return (
     <div className="absolute top-1/2 start-1/2 transform -translate-x-1/2 -translate-y-1/2">
       <div
-        className="animate-spin inline-block size-16 border-[3px] border-current border-t-transparent text-gray-900 rounded-full dark:text-gray-700"
+        className="animate-spin inline-block size-10 border-[4px] border-current border-t-transparent text-gray-900 rounded-full dark:text-gray-700"
         role="status"
         aria-label="loading"
       >
