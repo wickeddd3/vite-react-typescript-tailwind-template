e7ada5e280962b49984b6b87fee625e9a9e53114
wickeddd3 <pablophilipandrew@gmail.com>
2024-12-07 18:07:18 +0800

    feat: add main Routes

---
+++ src/router/Routes.tsx
@@ -0,0 +1,135 @@
+import { lazy } from "react";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import { MainLayout } from "@/components/layouts/main-layout/MainLayout";
+import { PageLoader } from "@/components/PageLoader";
+import { GuestGuard } from "@/router/guards/GuestGuard";
+import { AuthGuard } from "@/router/guards/AuthGuard";
+
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const NotFound = lazy(() => import("@/pages/NotFound"));
+
+const Login = lazy(() =>
+  delay(500).then(() => import("@/pages/auth/login/Login"))
+);
+const Register = lazy(() =>
+  delay(500).then(() => import("@/pages/auth/register/Register"))
+);
+
+const Dashboard = lazy(() =>
+  delay(500).then(() => import("@/pages/dashboard/Dashboard"))
+);
+const Analytics = lazy(() =>
+  delay(500).then(() => import("@/pages/analytics/Analytics"))
+);
+const Categories = lazy(() =>
+  delay(500).then(() => import("@/pages/ecommerce/categories/Categories"))
+);
+const Products = lazy(() =>
+  delay(500).then(() => import("@/pages/ecommerce/products/Products"))
+);
+const Users = lazy(() =>
+  delay(500).then(() => import("@/pages/accounts/users/Users"))
+);
+const Roles = lazy(() =>
+  delay(500).then(() => import("@/pages/accounts/roles/Roles"))
+);
+
+export const Routes = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      {
+        element: <GuestGuard />,
+        children: [
+          {
+            path: "login",
+            element: (
+              <PageLoader>
+                <Login />
+              </PageLoader>
+            ),
+          },
+          {
+            path: "register",
+            element: (
+              <PageLoader>
+                <Register />
+              </PageLoader>
+            ),
+          },
+        ],
+      },
+      {
+        element: <AuthGuard />,
+        children: [
+          {
+            element: <MainLayout />,
+            children: [
+              {
+                index: true, // This is the default route when the user visits "/"
+                element: <Navigate to="/dashboard" replace />,
+              },
+              {
+                path: "dashboard",
+                element: (
+                  <PageLoader>
+                    <Dashboard />
+                  </PageLoader>
+                ),
+              },
+              {
+                path: "analytics",
+                element: (
+                  <PageLoader>
+                    <Analytics />
+                  </PageLoader>
+                ),
+              },
+              {
+                path: "categories",
+                element: (
+                  <PageLoader>
+                    <Categories />
+                  </PageLoader>
+                ),
+              },
+              {
+                path: "products",
+                element: (
+                  <PageLoader>
+                    <Products />
+                  </PageLoader>
+                ),
+              },
+              {
+                path: "users",
+                element: (
+                  <PageLoader>
+                    <Users />
+                  </PageLoader>
+                ),
+              },
+              {
+                path: "roles",
+                element: (
+                  <PageLoader>
+                    <Roles />
+                  </PageLoader>
+                ),
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: (
+      <PageLoader>
+        <NotFound />
+      </PageLoader>
+    ),
+  },
+]);
