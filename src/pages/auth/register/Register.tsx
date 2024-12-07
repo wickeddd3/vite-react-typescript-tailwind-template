eed2647c76d2ced88aee64a9c42ca36f2dd84ee5
wickeddd3 <pablophilipandrew@gmail.com>
2024-12-07 18:08:12 +0800

    feat: add /login url

--- src/pages/auth/register/Register.tsx
+++ src/pages/auth/register/Register.tsx
@@ -34,7 +34,7 @@ const Register = () => {
           <p className="px-8 text-center text-sm text-muted-foreground">
             Already have an account?{" "}
             <Link
-              to="/"
+              to="/login"
               className="underline underline-offset-4 hover:text-primary"
             >
               Sign In
