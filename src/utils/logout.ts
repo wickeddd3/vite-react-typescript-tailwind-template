118e0b1ea181f50747f33ac10327554e19e04afd
wickeddd3 <pablophilipandrew@gmail.com>
2024-12-07 18:08:02 +0800

    feat: add /login as redirect route after logout

--- src/utils/logout.ts
+++ src/utils/logout.ts
@@ -5,6 +5,6 @@ export const logout = () => {
   // remove token from local storage
   removeLocalStorageState(tokenName);
   // reload and redirect to homepage
-  const hostUrl = `${window.location.protocol}//${window.location.host}`;
+  const hostUrl = `${window.location.protocol}//${window.location.host}/login`;
   window.location.href = hostUrl;
 };
