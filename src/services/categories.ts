626656ef631a464c2d89ca8ffa8b1fcbdb0be212
wickeddd3 <pablophilipandrew@gmail.com>
2024-12-07 23:24:05 +0800

    feat: add listCategories service error handling

--- src/services/categories.ts
+++ src/services/categories.ts
@@ -1,7 +1,15 @@
 import { apiRequest } from "@/lib/axios";
+import { AxiosError } from "axios";
 
 const categoriesResource = apiRequest({ url: "/api/categories" });
 
 export const listCategories = async () => {
-  return await categoriesResource.get();
+  try {
+    return await categoriesResource.get();
+  } catch (error) {
+    if (error instanceof AxiosError && error.response) {
+      return error.response;
+    }
+    throw new Error("An unexpected error occurred");
+  }
 };
