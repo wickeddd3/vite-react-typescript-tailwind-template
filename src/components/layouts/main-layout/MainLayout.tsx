0fee1fc69355831a34c17ed7619c8515522b8b84
wickeddd3 <pablophilipandrew@gmail.com>
2024-12-07 18:08:30 +0800

    cut: remove AuthGuard in MainLayout

--- src/components/layouts/main-layout/MainLayout.tsx
+++ src/components/layouts/main-layout/MainLayout.tsx
@@ -1,17 +1,14 @@
 import { MainSidebar } from "@/components/layouts/main-layout/MainSidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { Outlet } from "react-router-dom";
-import { AuthGuard } from "@/components/layouts/AuthGuard";
 
 export const MainLayout = () => {
   return (
-    <AuthGuard>
-      <SidebarProvider>
-        <MainSidebar />
-        <SidebarInset>
-          <Outlet />
-        </SidebarInset>
-      </SidebarProvider>
-    </AuthGuard>
+    <SidebarProvider>
+      <MainSidebar />
+      <SidebarInset>
+        <Outlet />
+      </SidebarInset>
+    </SidebarProvider>
   );
 };
