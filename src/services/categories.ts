8c64b815c098606d97af9d47c6ff9a1cf75313bd
wickeddd3 <pablophilipandrew@gmail.com>
2024-12-10 01:52:12 +0800

    feat: add create category service

--- src/services/categories.ts
+++ src/services/categories.ts
@@ -1,11 +1,24 @@
 import { apiRequest } from "@/lib/axios";
+import { CategorySchemaType } from "@/schema/category";
 import { AxiosError } from "axios";
 
-const categoriesResource = apiRequest({ url: "/api/categories" });
+const baseUrl = "/api/categories";
+const categoriesResource = apiRequest({ url: baseUrl });
 
-export const listCategories = async () => {
+export const list = async () => {
   try {
     return await categoriesResource.get();
+  } catch (error) {
+    if (error instanceof AxiosError && error.response) {
+      return error.response;
+    }
+    throw new Error("An unexpected error occurred");
+  }
+};
+
+export const create = async (data: CategorySchemaType) => {
+  try {
+    return await categoriesResource.post(data);
   } catch (error) {
     if (error instanceof AxiosError && error.response) {
       return error.response;
