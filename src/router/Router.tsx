1ebad59bda19aa8de3f3dc606476733ae4cbe3ca
wickeddd3 <pablophilipandrew@gmail.com>
2024-12-10 23:02:43 +0800

    cut: remove AuthRoutes, GuestRoutes, and Router

--- src/router/Router.tsx
+++
@@ -1,7 +0,0 @@
-import { RouterProvider } from "react-router-dom";
-import { AuthRoutes } from "@/router/AuthRoutes";
-import { GuestRoutes } from "@/router/GuestRoutes";
-
-export const Router = ({ token = "" }: { token: string | null }) => {
-  return <RouterProvider router={token ? AuthRoutes : GuestRoutes} />;
-};
