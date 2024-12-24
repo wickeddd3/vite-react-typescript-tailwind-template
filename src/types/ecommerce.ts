104e329f16b98f8cf3696e6fd68fb18182e75759
wickeddd3 <pablophilipandrew@gmail.com>
2024-12-24 16:35:14 +0800

    feat: update Product type

--- src/types/ecommerce.ts
+++ src/types/ecommerce.ts
@@ -8,10 +8,16 @@ export type Category = {
 
 export type Product = {
   id: number;
-  name: string;
   slug: string;
+  name: string;
+  brand: string;
+  supplier: string;
+  model: string;
+  serialNumber: string;
+  barcode: string;
   price: number;
   categoryId: number;
   createdAt: string;
   updatedAt: string;
+  category: Category;
 };
