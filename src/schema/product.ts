472ccfb2279b126d224b00f00ab1b5aec1fe8d02
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-05 17:27:45 +0800

    feat: update ProductSchema

--- src/schema/product.ts
+++ src/schema/product.ts
@@ -1,14 +1,23 @@
 import { z } from "zod";
 
 export const ProductSchema = z.object({
-  name: z.string().min(4),
+  name: z
+    .string({ required_error: "Name is required" })
+    .trim() // Trims whitespace from the input
+    .min(1, { message: "Name is required" }) // Ensures non-empty string
+    .max(150, { message: "Name must be at most 150 characters long" }),
   categoryId: z.number(),
   brand: z.string().optional(),
   supplier: z.string().optional(),
   model: z.string().optional(),
   serialNumber: z.string().optional(),
   barcode: z.string().optional(),
-  price: z.string().optional(),
+  price: z
+    .number({
+      required_error: "Price is required",
+      invalid_type_error: "Price must be a number",
+    })
+    .nonnegative(), // Ensures non-negative number
 });
 
 export type ProductSchemaType = z.infer<typeof ProductSchema>;
