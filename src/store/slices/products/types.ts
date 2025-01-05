bb3752d0465b77b88fab74b969929762511fca41
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-05 17:27:26 +0800

    feat: add optionCategories type

--- src/store/slices/products/types.ts
+++ src/store/slices/products/types.ts
@@ -1,9 +1,13 @@
-import { Product } from "@/types/ecommerce";
+import { Category, Product } from "@/types/ecommerce";
 
 export type ProductsState = {
   selectedProduct: Product | null;
   products: {
     data: Product[];
+    loading: boolean;
+  };
+  optionCategories: {
+    data: Category[];
     loading: boolean;
   };
 };
