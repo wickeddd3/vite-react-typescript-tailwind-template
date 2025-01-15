c55303b5e372f01b687c9cc773df4f8d02995845
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-16 00:58:26 +0800

    feat: add setProductsMetaReducer

--- src/store/slices/products/index.ts
+++ src/store/slices/products/index.ts
@@ -10,13 +10,17 @@ import {
 import { listCategoriesThunk } from "@/store/slices/products/thunks/product-options";
 import { productsReducers } from "@/store/slices/products/extra-reducers/products";
 import { productOptionsReducers } from "@/store/slices/products/extra-reducers/product-options";
-import { selectProductReducer } from "@/store/slices/products/reducers";
+import {
+  selectProductReducer,
+  setProductsMetaReducer,
+} from "@/store/slices/products/reducers";
 
 export const productsSlice = createSlice({
   name: "productsSlice",
   initialState,
   reducers: {
     selectProduct: selectProductReducer,
+    setProductsMeta: setProductsMetaReducer,
   },
   extraReducers(builder) {
     productsReducers(builder);
@@ -24,7 +28,7 @@ export const productsSlice = createSlice({
   },
 });
 
-export const { selectProduct } = productsSlice.actions;
+export const { selectProduct, setProductsMeta } = productsSlice.actions;
 
 export {
   listProductsThunk,
