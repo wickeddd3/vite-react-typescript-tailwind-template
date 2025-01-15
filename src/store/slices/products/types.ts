4148b4ac2bdd4147aaeba65017af0c41c71dfb57
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-16 00:58:02 +0800

    feat: add meta type of products state

--- src/store/slices/products/types.ts
+++ src/store/slices/products/types.ts
@@ -1,10 +1,12 @@
 import { Category, Product } from "@/types/ecommerce";
+import { Meta } from "@/types/table";
 
 export type ProductsState = {
   selectedProduct: Product | null;
   products: {
     data: Product[];
     loading: boolean;
+    meta: Meta;
   };
   optionCategories: {
     data: Category[];
