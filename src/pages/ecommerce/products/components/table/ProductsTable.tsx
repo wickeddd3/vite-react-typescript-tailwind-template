27c8bd4871cf6acb15ff9c4664d2bf60f37a4031
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-16 01:28:24 +0800

    feat: add meta, onUpdateMeta, and onPaginate props and pass it on Pagination component

--- src/pages/ecommerce/products/components/table/ProductsTable.tsx
+++ src/pages/ecommerce/products/components/table/ProductsTable.tsx
@@ -1,18 +1,33 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { DataTable } from "@/components/datatable/DataTable";
 import { Product } from "@/types/ecommerce";
+import { Pagination } from "./Pagination";
+import { Meta, PaginatedQuery } from "@/types/table";
 
 interface ProductsTableProps {
   columns: ColumnDef<Product>[];
   data: Product[];
+  meta: Meta;
+  onUpdateMeta: (meta: Meta) => void;
+  onPaginate: (query: PaginatedQuery) => void;
 }
 
-export const ProductsTable = ({ columns, data }: ProductsTableProps) => {
+export const ProductsTable = ({
+  columns,
+  data,
+  meta,
+  onUpdateMeta = () => {},
+  onPaginate = () => {},
+}: ProductsTableProps) => {
   return (
     <div className="space-y-4">
       {/* <Toolbar table={table} /> */}
       <DataTable columns={columns} data={data} />
-      {/* <Pagination table={table} /> */}
+      <Pagination
+        meta={meta}
+        onUpdateMeta={onUpdateMeta}
+        onPaginate={onPaginate}
+      />
     </div>
   );
 };
