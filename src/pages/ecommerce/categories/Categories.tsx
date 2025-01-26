cca567bac93e450a1dfec7956660ebe2fbfef629
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-26 23:17:16 +0800

    feat: add pagination based infinite scroll

--- src/pages/ecommerce/categories/Categories.tsx
+++ src/pages/ecommerce/categories/Categories.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/store";
 import { listCategoriesThunk } from "@/store/slices/categories";
@@ -6,16 +6,62 @@ import { Page } from "@/components/layouts/Page";
 import { CategorySearch } from "@/pages/ecommerce/categories/components/CategorySearch";
 import { CategoryList } from "@/pages/ecommerce/categories/components/CategoryList";
 import { AddCategoryButtonDialog } from "@/pages/ecommerce/categories/components/AddCategoryButtonDialog";
+import { LoadingDots } from "@/components/LoadingDots";
 
 const Categories = () => {
   const dispatch: AppDispatch = useDispatch();
 
+  const initialized = useSelector(
+    (state: RootState) => state.categoriesSlice.categories.initialized
+  );
   const categories = useSelector(
     (state: RootState) => state.categoriesSlice.categories.data
   );
+  const meta = useSelector(
+    (state: RootState) => state.categoriesSlice.categories.meta
+  );
+  const loading = useSelector(
+    (state: RootState) => state.categoriesSlice.categories.loading
+  );
+
+  const hasMore = meta.page < meta.totalPages;
+  const observer = useRef<IntersectionObserver | null>(null);
+
+  const handleLoadMoreData = () => {
+    if (!loading && hasMore) {
+      dispatch(
+        listCategoriesThunk({
+          page: meta.page + 1,
+          size: meta.size,
+          orderBy: "createdAt",
+          order: "desc",
+        })
+      );
+    }
+  };
+
+  const lastCategoryRef = (node: HTMLDivElement | null) => {
+    if (loading) return;
+
+    if (observer.current) observer.current.disconnect();
+    observer.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && hasMore) {
+        handleLoadMoreData();
+      }
+    });
+
+    if (node) observer.current.observe(node);
+  };
 
   useEffect(() => {
-    dispatch(listCategoriesThunk());
+    dispatch(
+      listCategoriesThunk({
+        page: 1,
+        size: 12,
+        orderBy: "createdAt",
+        order: "desc",
+      })
+    );
   }, [dispatch]);
 
   return (
@@ -31,7 +77,15 @@ const Categories = () => {
             <AddCategoryButtonDialog />
           </div>
         </div>
-        <CategoryList categories={categories} />
+        {initialized && (
+          <>
+            <CategoryList
+              categories={categories}
+              lastCategoryRef={lastCategoryRef}
+            />
+            {loading && <LoadingDots />}
+          </>
+        )}
       </Page.Content>
     </Page>
   );
--- src/pages/ecommerce/categories/components/CategoryList.tsx
+++ src/pages/ecommerce/categories/components/CategoryList.tsx
@@ -3,14 +3,24 @@ import { CategoryListItem } from "@/pages/ecommerce/categories/components/Catego
 
 interface CategoryListProps {
   categories: Category[];
+  lastCategoryRef: (node: HTMLDivElement | null) => void;
 }
 
-export const CategoryList = ({ categories }: CategoryListProps) => {
+export const CategoryList = ({
+  categories,
+  lastCategoryRef,
+}: CategoryListProps) => {
   return (
     <div className="flex flex-col gap-2">
-      {categories.map((category) => (
-        <CategoryListItem key={category.id} category={category} />
-      ))}
+      {categories.map((category, index) => {
+        const isLastCategory = index === categories.length - 1;
+
+        return (
+          <div ref={isLastCategory ? lastCategoryRef : null} key={category.id}>
+            <CategoryListItem category={category} />
+          </div>
+        );
+      })}
     </div>
   );
 };
