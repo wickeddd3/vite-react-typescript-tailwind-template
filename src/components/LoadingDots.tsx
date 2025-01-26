84f42ab86e31086f6650f2063030cd022c3bcad7
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-26 23:16:16 +0800

    feat: decrease loading dots size

--- src/components/LoadingDots.tsx
+++ src/components/LoadingDots.tsx
@@ -2,9 +2,9 @@ export const LoadingDots = () => {
   return (
     <div className="flex w-full h-fit space-x-2 justify-center items-center bg-white dark:invert">
       <span className="sr-only">Loading...</span>
-      <div className="h-4 w-4 bg-gray-900 rounded-full animate-bounce [animation-delay:-0.3s]"></div>
-      <div className="h-4 w-4 bg-gray-900 rounded-full animate-bounce [animation-delay:-0.15s]"></div>
-      <div className="h-4 w-4 bg-gray-900 rounded-full animate-bounce"></div>
+      <div className="h-2 w-2 bg-gray-900 rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+      <div className="h-2 w-2 bg-gray-900 rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+      <div className="h-2 w-2 bg-gray-900 rounded-full animate-bounce"></div>
     </div>
   );
 };
