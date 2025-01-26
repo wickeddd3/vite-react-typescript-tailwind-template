d39dadc8dad22d0ab2ce58ca641c03193e32e9e3
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-26 18:24:59 +0800

    cut: remove setProductsMetaReducer

--- src/store/slices/products/index.ts
+++ src/store/slices/products/index.ts
@@ -10,17 +10,13 @@ import {
 import { listCategoriesThunk } from "@/store/slices/products/thunks/product-options";
 import { productsReducers } from "@/store/slices/products/extra-reducers/products";
 import { productOptionsReducers } from "@/store/slices/products/extra-reducers/product-options";
-import {
-  selectProductReducer,
-  setProductsMetaReducer,
-} from "@/store/slices/products/reducers";
+import { selectProductReducer } from "@/store/slices/products/reducers";
 
 export const productsSlice = createSlice({
   name: "productsSlice",
   initialState,
   reducers: {
     selectProduct: selectProductReducer,
-    setProductsMeta: setProductsMetaReducer,
   },
   extraReducers(builder) {
     productsReducers(builder);
@@ -28,7 +24,7 @@ export const productsSlice = createSlice({
   },
 });
 
-export const { selectProduct, setProductsMeta } = productsSlice.actions;
+export const { selectProduct } = productsSlice.actions;
 
 export {
   listProductsThunk,
--- src/store/slices/products/reducers.ts
+++ src/store/slices/products/reducers.ts
@@ -1,18 +1,10 @@
 import { PayloadAction } from "@reduxjs/toolkit";
 import { ProductsState } from "@/store/slices/products/types";
 import { Product } from "@/types/ecommerce";
-import { Meta } from "@/types/table";
 
 export const selectProductReducer = (
   state: ProductsState,
   action: PayloadAction<Product | null>
 ) => {
   state.selectedProduct = action.payload;
-};
-
-export const setProductsMetaReducer = (
-  state: ProductsState,
-  action: PayloadAction<Meta>
-) => {
-  state.products.meta = action.payload;
 };
