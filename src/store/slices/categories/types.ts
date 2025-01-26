8f54eff0e8487ae9ea20bbd5d6303ec2cd60eb88
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-26 23:14:05 +0800

    feat: add meta and initialized type

--- src/store/slices/categories/types.ts
+++ src/store/slices/categories/types.ts
@@ -1,9 +1,12 @@
 import { Category } from "@/types/ecommerce";
+import { Meta } from "@/types/table";
 
 export type CategoriesState = {
   selectedCategory: Category | null;
   categories: {
     data: Category[];
     loading: boolean;
+    initialized: boolean;
+    meta: Meta;
   };
 };
